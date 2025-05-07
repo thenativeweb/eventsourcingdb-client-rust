a4129f0b5be5bb8d7f42d1d01bb8021476da54e9
Raphael Höser <raphael.hoeser@aptiv.com>
2025-05-07 10:09:08 +0200

    chore: update warnings to use deny instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,16 @@
 #![doc = include_str!("../README.md")]
 
-#![warn(
+#![deny(
     ambiguous_negative_literals,
-    bad_style,
     clippy::pedantic,
     missing_debug_implementations,
     missing_docs,
-    non_shorthand_field_patterns,
-    overflowing_literals,
-    path_statements,
     trivial_casts,
     trivial_numeric_casts,
     unused_extern_crates,
     unused_import_braces,
     unused_qualifications,
-    unused_results
+    unused_results,
+    unsafe_code,
+    warnings
 )]
-#![deny(unsafe_code)]
